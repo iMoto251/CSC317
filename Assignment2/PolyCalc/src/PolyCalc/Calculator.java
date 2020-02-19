7a6667505a8374e9c9442d2ae3b6193687bb9b12
iMoto251 <55514345+iMoto251@users.noreply.github.com>
2020-02-18 21:47:23 -0600

    added switch

--- Assignment2/PolyCalc/src/PolyCalc/Calculator.java
+++ Assignment2/PolyCalc/src/PolyCalc/Calculator.java
@@ -1,10 +1,38 @@
 package PolyCalc;
 
+//import java.io.*;
+import java.util.*;
+
 public class Calculator {
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print("1. Addition\n2. Multiplication\n3. Evaluation\n4. Derivative\n5. Exit\nEnter Choice: ");
+		Scanner choice = new Scanner(System.in);
+		
+		int selection = choice.nextInt();
+		//while loop for switch
+		while(selection != 5) {
+			//switch to decide which function to run
+			switch(selection) {
+			case 1: System.out.println("C1");
+					break;
+			case 2: System.out.println("C2");
+					break;
+			case 3: System.out.println("C3");
+					break;
+			case 4: System.out.println("C4");
+					break;
+			default:
+					System.out.println("Invalid choice");
+					break;
+			}
+			//asks for selection to loop around to the top if the switch
+        	System.out.print("1. Addition\n2. Multiplication\n3. Evaluation\n4. Derivative\n5. Exit\nEnter Choice: ");
+        	selection = choice.nextInt();
+		}
+		
+		
+		choice.close();
 	}
 	
 }

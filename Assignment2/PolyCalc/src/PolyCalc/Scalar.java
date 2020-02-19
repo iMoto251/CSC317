60bba0e8318c7afffe2ec610084ea8814b05393e
iMoto251 <55514345+iMoto251@users.noreply.github.com>
2020-02-18 19:57:41 -0600

    Add files via upload

---
+++ Assignment2/PolyCalc/src/PolyCalc/Scalar.java
@@ -0,0 +1,32 @@
+package PolyCalc;
+
+public class Scalar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void add(Scalar s) {
+		
+	}
+	
+	public void mult(Scalar s) {
+		
+	}
+	
+	public void pow(int exp) {
+		
+	}
+	
+	public void neg() {
+		
+	}
+	
+	public boolean equals(Scalar s) {
+		
+		
+		return false;
+	}
+
+}

41d21cf9aaea0d3811e2af441c88970ffeb2c3c8
iMoto251 <55514345+iMoto251@users.noreply.github.com>
2020-02-19 10:18:44 -0600

    Update Calculator.java

--- Assignment2/PolyCalc/src/PolyCalc/Calculator.java
+++ Assignment2/PolyCalc/src/PolyCalc/Calculator.java
@@ -15,16 +15,15 @@ public class Calculator {
 			//switch to decide which function to run
 			switch(selection) {
 			case 1: System.out.println("C1");
-					break;
+				break;
 			case 2: System.out.println("C2");
-					break;
+				break;
 			case 3: System.out.println("C3");
-					break;
+				break;
 			case 4: System.out.println("C4");
-					break;
-			default:
-					System.out.println("Invalid choice");
-					break;
+				break;
+			default:System.out.println("Invalid choice");
+				break;
 			}
 			//asks for selection to loop around to the top if the switch
         	System.out.print("1. Addition\n2. Multiplication\n3. Evaluation\n4. Derivative\n5. Exit\nEnter Choice: ");

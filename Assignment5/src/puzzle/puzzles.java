aa6a5a5bd3e3c4972099f079b2fa1f74a9453545
iMoto251 <55514345+iMoto251@users.noreply.github.com>
2020-04-27 21:41:35 -0500

    update

---
+++ Assignment5/src/puzzle/puzzles.java
@@ -0,0 +1,31 @@
+package puzzle;
+
+import java.io.*;
+import java.util.*;
+
+public class puzzles {
+
+	public static void main(String args[]) {
+		System.out.print("1. Eight Puzzle\n2. Pitchers Puzzle\n3. Exit\nSelect a game: ");
+		Scanner selection = new Scanner(System.in);
+		String s = selection.next();
+		
+		if(s.equals("1")) {
+			System.out.println(" ");
+			eightPuzzle.game();
+		}
+		else if(s.equals("2")) {
+			System.out.println("Game 2");
+		}
+		else {
+			System.out.println("Invalid Selection");
+		}
+		
+		
+		
+		
+		selection.close();
+	}
+
+	
+}

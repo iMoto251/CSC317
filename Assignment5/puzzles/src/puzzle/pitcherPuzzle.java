33e6a997c574dea295c7f062aa6e74cd876d1e4f
iMoto251 <55514345+iMoto251@users.noreply.github.com>
2020-05-04 11:22:11 -0500

    Update pitcherPuzzle.java
    working

--- Assignment5/puzzles/src/puzzle/pitcherPuzzle.java
+++ Assignment5/puzzles/src/puzzle/pitcherPuzzle.java
@@ -94,19 +94,18 @@ public class pitcherPuzzle {
 
 	
 	public static void game() {
-		
+		//Number of pitchers
 		System.out.print("Enter the number of pitchers: "); 
 		Scanner pitchNum = new Scanner(System.in); //Read in number of pitchers
 		int pitchers = pitchNum.nextInt();	
 		int cap[] = new int[pitchers]; //capacity of pitchers
 		int level[] = new int[pitchers]; //amounts in the pitchers
 		int y=0;
-		
+				
+		//Capacities of the pitchers
 		System.out.println("Enter the capacities of the " + pitchers + " (gallons) [x,x,x,...,n]: ");
 		Scanner entry = new Scanner(System.in);
 		String input = entry.nextLine();
-		
-		//not working
 		if(input.charAt(0) == '[') {
 			for(int i=0;i<input.length();i++) {
 				if(input.charAt(i) == ']') {
@@ -114,41 +113,16 @@ public class pitcherPuzzle {
 				}
 				else if(Character.isDigit(input.charAt(i+1))) {
 					cap[y] = Character.getNumericValue(input.charAt(i+1));
-				}
-			}y++;
-			//System.out.println(input);
-		}
-		
-		for(int i=0;i<pitchers;i++)
-			System.out.println(cap[i]);
-			
-		
-		
-		/*
-		 if(input.charAt(0) == '[') {
-			for(int i=0;i<input.length();i++) {
-				if(input.charAt(i) == ',' && i!=input.length()-1) {
-					if(Character.isDigit(input.charAt(i+1))){
-						cap[i] = Character.getNumericValue(input.charAt(i+1));						
-					}
+					y++;
 				}
 			}
 		}
 		
-		//Working, but not how Banerjee wants it.
-		System.out.println("Enter the capacities of the " + pitchers + " on seperate lines (gallons)");
-		Scanner entry = new Scanner(System.in);
-		for(int i =0; i< pitchers; i++) {
-			if(i != pitchers) {
-				System.out.print("Pitcher " + (i+1) + ": ");
-				int gallons = entry.nextInt();
-				cap[i] = gallons;
-			}			
-		}
-		*/
-		
+		//
 		System.out.print("Enter the goal (gallons): ");
 		int goal = entry.nextInt();
+		
+		
 		disp(level);
 		Scanner sc = new Scanner(System.in);
 		while (true) {
@@ -157,15 +131,12 @@ public class pitcherPuzzle {
 			ArrayList<String> choiceArray = dispChoices(cap, level);
 			int s=sc.nextInt();
 					
-			String pick = choiceArray.get((s-1));
-			System.out.println(pick);
-			
+			String pick = choiceArray.get((s-1));			
 			char function = pick.charAt(0);
 			int pitcher1 = Character.getNumericValue(pick.charAt((pick.length()) - 2) - 1);
 			int pitcher2 = Character.getNumericValue(pick.charAt((pick.length()) - 7) - 1);
 
 			
-			//System.out.println(function + " " + pitcher1 + " " + pitcher2);
 		
 			if(function == 'F') {
 				fill(cap[pitcher1],pitcher1,level);
